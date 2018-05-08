faff3ac9cc194e37f54b758c1424cc300890df7f
Mia Šimunić <miasimuni@foi.hr>
2018-05-08 02:10:21 +0200

    Initial desigining

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<DOCTYPE!>
+<html>
+    <head>
+        <title> Cassiopeia - shopping out of this world!</title>
+
+        <link rel="stylesheet" type="text/css" href="css/miasimuni.css">
+        <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext" rel="stylesheet">
+
+
+    </head>
+    <body>
+        <header class="title-row">
+            <div class="casssiopeia-logo">&nbsp;</div>
+
+        </header>
+        <nav class="navigation-bar">
+            <a href="prijavaRegistracija.php">Prijava</a>
+
+        </nav>
+
+    </body>
+
+</html>
